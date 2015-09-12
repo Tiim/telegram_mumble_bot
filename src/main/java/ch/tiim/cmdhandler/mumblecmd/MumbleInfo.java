930e497151fecc473c5a21157c10b0d8a3f06728
Tiim <tim.bachmann@cad.ch>
2015-09-12 16:33:25 +0200

    Added a special message when nobody is currently on mumble.
    Thx to @Teapex for pointing this out.

--- src/main/java/ch/tiim/cmdhandler/mumblecmd/MumbleInfo.java
+++ src/main/java/ch/tiim/cmdhandler/mumblecmd/MumbleInfo.java
@@ -7,6 +7,7 @@ import ch.tiim.telegram.TGMessage;
 import ch.tiim.telegram.TelegramBot;
 
 import java.io.IOException;
+import java.util.Collection;
 
 public class MumbleInfo implements CmdHandler {
 
@@ -24,8 +25,14 @@ public class MumbleInfo implements CmdHandler {
     @Override
     public void handleMessage(String[] cmd, TGMessage m, TelegramBot b) throws IOException {
         StringBuilder sb = new StringBuilder();
-        sb.append("Following users are currently online:\n");
-        for (User u : mumble.getMumbleUsers()) {
+        Collection<User> users = mumble.getMumbleUsers();
+        if (users.size() != 0) {
+            sb.append("Following users are currently online:\n");
+        } else {
+            sb.append("\u0302\u030eN\u036a\u034eo\u035b\u0360b\u035b\u0304o\u0367\u0347d\u0321\u0305 y\u0303\u036b is currently online\uD83D\uDE13\n");
+            sb.append("Be the \uD83C\uDD75\uD83C\uDD78\uD83C\uDD81\uD83C\uDD82\uD83C\uDD83 to join and make everyone follow you\uD83D\uDE0C");
+        }
+        for (User u : users) {
             sb.append("\uD83D\uDD38");
             sb.append(u.name).append(" (");
             if (u.mute || u.selfMute) {
